ae1927763ad84aa7330cfe7490523b025c2926bf
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-10-06 18:39:20 +0200

    première version

---
+++ views/layout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <title>Some relevant title</title>
+</head>
+
+<body>
+  <header>
+    <a href='/php_mvc_blog'>Home</a>
+    <a href='?controller=posts&action=index'>Posts</a>
+  </header>
+
+  <?php require_once('routes.php'); ?>
+
+  <footer>
+    Copyright
+  </footer>
+
+  <body>
+
+</html>
