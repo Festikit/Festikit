80f3d94c00a1c3ed80d68af1387c0fbd4a8724bb
Soysal Mathieu <mathieu.soysal@outlook.fr>
2020-10-31 22:13:15 +0100

    Ajout : gitlabci.yml:  - Déploiement pour production  - Déploiement pour staging

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <title> Mon premier php </title>
+</head>
+
+<body>
+    Voici le résultat du script PHP :
+    <?php
+    $texte = "hello world !";
+    echo $texte;
+    ?>
+</body>
+
+</html>
