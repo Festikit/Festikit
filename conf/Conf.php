6e2e39705a4da1a0865a6f244dfb601dba181f26
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-10-31 23:32:19 +0100

    Ajout : Conf

---
+++ conf/Conf.php
@@ -0,0 +1,33 @@
+<?php
+
+class Conf {
+   
+  static private $debug = true;
+
+  static private $databases = array(
+    'hostname' => 'webinfo',
+    'database' => 'cours_php',
+    'login' => 'root',
+    'password' => 'root'
+  );
+   
+  static public function getLogin() {
+    return self::$databases['login'];
+  }
+
+  static public function getHostname() {
+    return self::$databases['hostname'];
+  }
+
+  static public function getDatabase() {
+    return self::$databases['database'];
+  }
+
+  static public function getPassword() {
+    return self::$databases['password'];
+  }
+
+  static public function getDebug() {
+    return self::$debug; 
+  }
+}
