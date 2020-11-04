23cd3d4a9edbb5070a63cdc20a5b46d552d5bacb
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-11-04 20:35:35 +0100

    Mise à jour : create.php

--- view/utilisateur/create.php
+++ view/utilisateur/create.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr" xml:lang="fr">
-
-<head>
-    <title>Formulaire</title>
-    <meta charset="utf-8">
-    <!-- Materialize: Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">
-    </script>
-
-    <script>
-        $(document).ready(function() {
-            $('select').material_select();
-        });
-    </script>
-</head>
-
 <body class="grey lighten-3">
     <div class="row">
         <div class="col s12 m4 l2">
@@ -467,8 +446,6 @@
             </form>
         </div>
     </div>
-    <!-- Compiled and minified JavaScript -->
-
 </body>
 
 </html>
