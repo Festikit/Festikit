6c7cde367dff6dc72fc558d32eda7465c9f18d16
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-11-07 21:44:51 +0100

    mise à jour : update.php

--- view/utilisateur/update.php
+++ view/utilisateur/update.php
@@ -42,14 +42,14 @@ if (!($log_u == "" && $user_firstname == "" && $user_lastname == "" && $user_mai
             <div class="row">
                 <div class="input-field col s6">
                     <i class="material-icons prefix">insert_invitation</i>
-                <?php echo '<input id="date_naissance" type="date" max="2010-01-01" min="1900-01-01" class="validate" value="' . rawurlencode($user_birthdate) . '" name="user_birthdate" required>'; ?>
-                    <label for="date_naissance" class="active">date de naissance</label>
+                <?php echo '<input name"user_birthdate" id="user_birthdate" type="date" max="2010-01-01" min="1900-01-01" class="validate" value="' . rawurlencode($user_birthdate) . '" name="user_birthdate" required>'; ?>
+                    <label for="user_birthdate" class="active">date de naissance</label>
                 </div>
                 <div class="file-field input-field col s6">
                     <div class="btn">
                         <i class="material-icons">file_download</i>
                         <span>Photo de profil</span>
-                        <input id="avatar" type="file" accept="image/png, image/jpeg">
+                        <input name="user_picture" id="user_picture" type="file" accept="image/png, image/jpeg">
                     </div>
                     <div class="file-path-wrapper">
                         <input class="file-path validate" type="text">
@@ -60,9 +60,8 @@ if (!($log_u == "" && $user_firstname == "" && $user_lastname == "" && $user_mai
             <div class="row">
                 <div class="input-field col s6">
                     <i class="material-icons prefix">person_pin</i>
-                    <?php echo '<input id="code_postal" type="number" value="' .rawurlencode($user_postal). '" class="validate" required>'; ?>
-                    <input id="code_postal" type="number" value="84450" class="validate" required>
-                    <label for="code_postal" class="active">Code Postal</label>
+                    <?php echo '<input id="user_postal_code" type="number" value="' .rawurlencode($user_postal_code). '" class="validate" required>'; ?>
+                    <label for="user_postal_code" class="active">Code Postal</label>
                 </div>
                 <div class="input-field col s6">
                     <i class="material-icons prefix">phone</i>
