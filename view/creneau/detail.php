a26755f19daefb90fde20678a72a031d14f3700e
Pablo-Davier <pablo.davier@etu.umontpellier.fr>
2020-11-14 20:24:53 +0100

    amelioration des deux vues R

--- view/creneau/detail.php
+++ view/creneau/detail.php
@@ -4,7 +4,10 @@
 echo "<p> Le créneaux d'Id ". htmlspecialchars($c->getCreneauId()) . 
     " commence le : ". htmlspecialchars($c->getCreneauStart()) . 
     " et finit le : " . htmlspecialchars($c->getCreneauEnd()) . "</p>" .
-    "<p> Il est assigné : <br> - au festival d'Id : ". htmlspecialchars($c->getFestivalId()) . "<br>" .
-    "- au poste d'Id : ". htmlspecialchars($c->getPosteId()) . "</p>";
+    "<p> Il est assigné : <br>" . 
+    "- au festival d'Id : <a href=\"index.php?action=read&controller=festival&festival_id=" . rawurlencode($c->getFestivalId()) . "\">" . htmlspecialchars($c->getFestivalId()) . "</a><br>" .
+    "- au poste d'Id : <a href=\"index.php?action=read&controller=poste&poste_id=" . rawurlencode($c->getPosteId()) . "\">" . htmlspecialchars($c->getPosteId()) . "</a></p>";
 
 echo "<p> Retour: <a href=\"index.php?action=readAll&controller=$controller\">Cliquez ici </a> </p>";
+
+
