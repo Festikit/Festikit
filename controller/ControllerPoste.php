ecee475c94c3584b280bafd954cadbb7d181f5ea
Pablo-Davier <pablo.davier@etu.umontpellier.fr>
2020-12-01 18:08:49 +0100

    Create poste + delete & deleted

--- controller/ControllerPoste.php
+++ controller/ControllerPoste.php
@@ -63,6 +63,50 @@ class ControllerPoste {
         $tab_p = ModelPoste::selectAll();
         require (File::build_path(array("view","view.php")));
     }
+
+    public static function create()
+    {
+        $controller = 'poste';
+        $view = 'create';
+        $pagetitle = 'ajout d\'un poste';
+
+        require(File::build_path(array("view", "view.php")));
+    }
+
+    public static function created()
+    {
+        $controller = 'poste';
+        $view = 'create';
+        $pagetitle = 'ajout d\'un poste';
+
+        $poste_name = $_GET['poste_name'];
+        $poste_description = $_GET['poste_description'];
+        $festival_id = $_GET['festival_id'];        
+
+        $dataPoste = array(
+            "poste_name" => $poste_name,
+            "poste_description" => $poste_description,
+            "festival_id" => $festival_id,
+            
+        );
+    
+        if (is_bool(ModelPoste::save($dataPoste))) {
+            $controller = 'creneau';
+            $view = 'error';
+            $message = 'Erreur: Insertion des données dans la table poste';
+            $pagetitle = 'erreur';
+        }
+        require(File::build_path(array("view", "view.php")));
+    }
+
+    public static function delete(){
+        $controller = 'poste';
+        $pagetitle = 'supprimons ceci';
+        $view = 'deleted';
+        $poste_id = $_GET['poste_id'];
+        ModelPoste::delete($poste_id);
+        require File::build_path(array("view","view.php"));
+    }
 }
 
 ?>
