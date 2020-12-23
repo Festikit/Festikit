82f5b57f3479bcc9880f8cc44d8a54f392c806d6
Albaut Bastian <bastian.albaut@etu.umontpellier.fr>
2020-12-23 01:05:22 +0100

    ModelPostuler.php maj

---
+++ model/ModelPostuler.php
@@ -0,0 +1,62 @@
+<?php
+require_once File::build_path(array("model", "Model.php"));
+
+class ModelPostuler extends Model
+{
+  private $postuler_id;
+  private $user_id;
+  private $festival_id;
+  private $postuler_accepted;
+  private $venir_avec_vehicule;
+  private $besoin_hebergement;
+  private $peut_heberger;
+  private $configuration_couchage;
+  private $arrivee_festival;
+  private $depart_festival;
+  private $autres_dispos;
+  private $experience;
+
+
+  protected static $object_table = 'postuler';
+  protected static $object_model = 'postuler';
+  protected static $primary= 'postuler_id';
+
+  public function __construct($id = NULL, $user_id = NULL, $festival_id = NULL, $postuler_accepted = NULL)
+  {
+    if (!is_null($id) && !is_null($user_id) && !is_null($festival_id) && !is_null($postuler_accepted)) {
+      $this->postuler_id = $id;
+      $this->user_id = $user_id;
+      $this->festival_id = $festival_id;
+      $this->postuler_accepted = $postuler_accepted;
+    }
+  }
+
+  // Getter et Setter: poste_id
+  public function getPostulerId()
+  {
+    return $this->postuler_id;
+  }
+  public function setPostulerId($postuler_id2)
+  {
+    $this->postuler_id = $postuler_id2;
+  }
+
+  /*
+  public static function getAllPostuler()
+  {
+    try {
+      $sql = "SELECT * from postuler";
+      $rep = Model::$pdo->query($sql);
+      $rep->setFetchMode(PDO::FETCH_CLASS, 'ModelPostuler');
+      return $rep->fetchAll();
+    } catch (PDOException $e) {
+      if (Conf::getDebug()) {
+        echo $e->getMessage();
+      } else {
+        echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+      }
+      die();
+    }
+  }
+  */
+}
