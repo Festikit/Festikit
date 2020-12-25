788ed7c2a48e235cccbfbffc36f2d789196b2e5a
Bastian-alb <bastian.albaut@etu.umontpellier.fr>
2020-12-25 20:53:07 +0100

    session timeout

--- index.php
+++ index.php
@@ -8,4 +8,12 @@ require_once File::build_path(array('lib', 'Security.php'));
 
 require_once File::build_path(array("controller","routeur.php"));
 
+// Timeout sur session
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > (30*60))) { // Dernière activité datant de moins de 30min
+    session_unset();
+    session_destroy();
+ } else {
+    $_SESSION['LAST_ACTIVITY'] = time(); // Mise à jour de la dernière activité
+ }
+
 ?>
