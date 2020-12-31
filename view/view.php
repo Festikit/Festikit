2dc43b84842f85aa78d56d1c17fd9fe0035eb1d4
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-12-31 16:58:43 +0100

    Ajout icone

--- view/view.php
+++ view/view.php
@@ -11,7 +11,13 @@
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">
 	</script>
-
+	<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+	<link rel="manifest" href="site.webmanifest">
+	<link rel="mask-icon" href="safari-pinned-tab.svg" color="#d55b5d">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#fff8f8">
 	<script>
 		$(document).ready(function() {
 			$('select').material_select();
