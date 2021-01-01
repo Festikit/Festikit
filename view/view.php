d8c3d38957e1b0c07bbf4264add773cfdbe331ac
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-01-01 02:10:06 +0100

    Mise à jour Jquery + Materielize

--- view/view.php
+++ view/view.php
@@ -8,8 +8,8 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js">
 	</script>
 	<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
@@ -20,7 +20,7 @@
 	<meta name="theme-color" content="#fff8f8">
 	<script>
 		$(document).ready(function() {
-			$('select').material_select();
+			$('.modal').modal();
 		});
 
 		/* Vérification Mot de passe */
