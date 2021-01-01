40ed9f9f29566beac68c0b74369bde88af3ff237
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-01-01 19:58:54 +0100

    Mise à navbar : responsiv mobile + petit cadeau

--- view/view.php
+++ view/view.php
@@ -2,12 +2,14 @@
 <html lang="fr" xml:lang="fr">
 
 <head>
-	<title><?php echo $pagetitle; ?></title>
+	<title>
+		<?php echo $pagetitle; ?>
+	</title>
 	<meta charset="utf-8">
 	<!-- Materialize: Compiled and minified CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js">
 	</script>
@@ -19,7 +21,8 @@
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#fff8f8">
 	<script>
-		$(document).ready(function() {
+		$(document).ready(function () {
+			$('.sidenav').sidenav();
 			$('.modal').modal();
 		});
 
@@ -49,7 +52,7 @@
 			}
 		}
 
-		$(document).ready(function() {
+		$(document).ready(function () {
 			$("#user_password1").keyup(checkPasswordLength);
 		});
 
@@ -63,7 +66,7 @@
 				$("#checkPasswordMatch").html("Mots de passe identiques");
 		}
 
-		$(document).ready(function() {
+		$(document).ready(function () {
 			$("#user_password2").keyup(checkPasswordMatch);
 		});
 	</script>
@@ -71,7 +74,7 @@
 		main {
 			padding-top: 10vh;
 			padding-bottom: 10vh;
-			min-height: 60vh;
+			min-height: 70vh;
 			place-content: center;
 			gap: 1ch;
 		}
@@ -81,9 +84,12 @@
 <body>
 	<!-- HEADER -->
 	<header>
-		<nav>
+		<nav height="64px">
 			<div class="nav-wrapper grey">
-				<a href="#" class="brand-logo">🎈</a>
+				<a href="#" class=""><img src="rakhi.svg" alt="icone du site" width="57px" height="57px"><span
+						class="brand-logo">Festivanus</span></a>
+				<a href="#" data-target="mobile-demo" class="sidenav-trigger right"><i
+						class="material-icons">menu</i></a>
 				<ul id="nav-mobile" class="right hide-on-med-and-down">
 					<?php
 					if (Session::is_admin()) {
@@ -123,6 +129,44 @@
 				</ul>
 			</div>
 		</nav>
+
+		<ul class="sidenav" id="mobile-demo">
+			<?php
+			if (Session::is_admin()) {
+				echo '
+						<li><a href="index.php?action=readAll">Accueil Utilisateur</a></li>
+						<li><a href="index.php?action=readAll&controller=festival">Accueil Festival</a></li>
+						<li><a href="index.php?action=readAll&controller=responsable">Accueil Responsable</a></li>
+						';
+			}
+			if (Session::is_responsable()) {
+				echo '
+						<li><a href="index.php?action=readAll&controller=festival">Accueil Festival</a></li>
+						';
+			}
+			if (!Session::is_admin() && !Session::is_responsable()) {
+				echo '
+						<li><a href="index.php?action=readAll&controller=festival">Inscription</a></li>
+						';
+			}
+			if (empty($_SESSION['login'])) {
+				echo '
+						
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=connect">Connexion</a>
+                    </li>
+                    ';
+			} else {
+				echo '
+						<li class="nav-item">
+							<a class="nav-link" href="index.php?action=read&user_id=' . $_SESSION['login'] . '">Votre Compte</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="index.php?action=deconnect">Déconnexion</a>
+						</li>';
+			}
+			?>
+		</ul>
 	</header>
 
 	<main>
@@ -139,20 +183,30 @@
 				<div class="col l6 s12">
 					<h5 class="white-text">Besoin d'aider ? Contactez-nous !</h5>
 					<p class="grey-text text-lighten-4">
-						<h6>
-							mail : <a class="grey-text text-lighten-2" href="mailto:mathieu.soysal+aidefestival@outlook.fr">support@GestionnaireFestival.fr</a>
-						</h6>
-						<h6>
-							tel : <a class="grey-text text-lighten-2" href="tel:+33652600110">065260001</a>
-						</h6>
+					<h6>
+						mail : <a class="grey-text text-lighten-2"
+							href="mailto:mathieu.soysal+aidefestival@outlook.fr">support@GestionnaireFestival.fr</a>
+					</h6>
+					<h6>
+						tel : <a class="grey-text text-lighten-2" href="tel:+33652600110">065260001</a>
+					</h6>
 					</p>
 				</div>
 				<div class="col l4 offset-l2 s12">
 					<h5 class="white-text">Partager</h5>
 					<ul>
-						<li> <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fbenevoles.herokuapp.com" data-rel="popup" rel="nofollow" title="Partager sur Facebook" role="button" aria-label="Partager sur Facebook"><span class="fab fa-facebook-f"></span><span class="grey-text text-lighten-3">Facebook</span></a></li>
-						<li><a href="https://twitter.com/intent/tweet?text=Gestionnaire%20de%20festival%20en%20ligne&amp;url=https%3A%2F%2Fbenevoles.herokuapp.com" data-rel="popup" rel="nofollow" title="Partager sur Twitter" role="button" aria-label="Partager sur Twitter"><span class="fab fa-twitter"></span><span class="grey-text text-lighten-3">Tweeter</span></a></li>
-						<li><a href="https://www.linkedin.com/shareArticle?mini=true&amp;summary=Un%20moyen%20simple%20et%20efficace%20de%20faciliter%20la%20collecte%20des%20informations%20personnelles%2C%20des%20disponibilit%C3%A9s%20et%20des%20pr%C3%A9f%C3%A9rences%20de%20poste%20de%20chacun%20des%20b%C3%A9n%C3%A9voles.&amp;title=Gestionnaire%20de%20festival%20en%20ligne&amp;url=https%3A%2F%2Fbenevoles.herokuapp.com" data-rel="popup" rel="nofollow" title="Partager sur LinkedIn" role="button" aria-label="Partager sur LinkedIn"><span class="fab fa-linkedin-in"></span><span class="grey-text text-lighten-3">LinkedIn</span></a></li>
+						<li> <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fbenevoles.herokuapp.com"
+								data-rel="popup" rel="nofollow" title="Partager sur Facebook" role="button"
+								aria-label="Partager sur Facebook"><span class="fab fa-facebook-f"></span><span
+									class="grey-text text-lighten-3">Facebook</span></a></li>
+						<li><a href="https://twitter.com/intent/tweet?text=Gestionnaire%20de%20festival%20en%20ligne&amp;url=https%3A%2F%2Fbenevoles.herokuapp.com"
+								data-rel="popup" rel="nofollow" title="Partager sur Twitter" role="button"
+								aria-label="Partager sur Twitter"><span class="fab fa-twitter"></span><span
+									class="grey-text text-lighten-3">Tweeter</span></a></li>
+						<li><a href="https://www.linkedin.com/shareArticle?mini=true&amp;summary=Un%20moyen%20simple%20et%20efficace%20de%20faciliter%20la%20collecte%20des%20informations%20personnelles%2C%20des%20disponibilit%C3%A9s%20et%20des%20pr%C3%A9f%C3%A9rences%20de%20poste%20de%20chacun%20des%20b%C3%A9n%C3%A9voles.&amp;title=Gestionnaire%20de%20festival%20en%20ligne&amp;url=https%3A%2F%2Fbenevoles.herokuapp.com"
+								data-rel="popup" rel="nofollow" title="Partager sur LinkedIn" role="button"
+								aria-label="Partager sur LinkedIn"><span class="fab fa-linkedin-in"></span><span
+									class="grey-text text-lighten-3">LinkedIn</span></a></li>
 					</ul>
 				</div>
 			</div>
