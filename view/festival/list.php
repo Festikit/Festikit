3a7eeceeb945236e0b533c228bf9dee9d0d3efe4
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-01-03 21:29:41 +0100

    Mise à jour design : liste festival

--- view/festival/list.php
+++ view/festival/list.php
@@ -1,15 +1,17 @@
-<ul class="collection">
-	<li class="collection-header">
-		<?php
-		if (!$boolResponsable) {
-			echo '<a class="btn-large waves-effect waves-light secondary-content" href="index.php?action=create&controller=festival"> Ajouter un festival</a>';
-			echo "<h4>Liste de tous les festivals</h4>
+<div class="row">
+	<div class="card col l8 offset-l2 col m10 offset-m1 col s12">
+		<ul class="collection">
+			<li class="collection-header">
+				<?php
+				if (!$boolResponsable) {
+					echo '<a class="btn-large waves-effect waves-light secondary-content" href="index.php?action=create&controller=festival"> Ajouter un festival</a>';
+					echo "<h4>Liste de tous les festivals</h4>
 		</li>";
-			$i = 1;
-			foreach ($tab_f as $f) {
-				$festival_name = htmlspecialchars($f->getFestivalName());
-				$festival_id = $f->getFestivalId();
-				echo "
+					$i = 1;
+					foreach ($tab_f as $f) {
+						$festival_name = htmlspecialchars($f->getFestivalName());
+						$festival_id = $f->getFestivalId();
+						echo "
 				<li class=\"collection-item\">
 					<a href=\"index.php?action=read&controller=festival&festival_id=$festival_id\"> <span class=\"title\">$festival_name</span></a>
 					<div class=\"secondary-content\">
@@ -17,44 +19,46 @@
 						<a title=\"voir le formulaire\" href=\"index.php?action=create&festival_id=$festival_id\" class=\"btn\"><i class=\"material-icons\">assignment</i></a>
 					</div>
 				</li>";
-				$i++;
-			}
-		}
+						$i++;
+					}
+				}
 
 
-		if ($boolResponsable) {
-			echo '<h4>Liste des festivals en tant que responsable</h4>';
-			$i = 1;
-			foreach ($tab_r as $r) {
-				$festival_name = htmlspecialchars($r->getFestivalName());
-				$festival_id = $r->getFestivalId();
-				echo "<li class=\"collection-item\">
+				if ($boolResponsable) {
+					echo '<h4>Liste des festivals en tant que responsable</h4>';
+					$i = 1;
+					foreach ($tab_r as $r) {
+						$festival_name = htmlspecialchars($r->getFestivalName());
+						$festival_id = $r->getFestivalId();
+						echo "<li class=\"collection-item\">
 		<a href=\"index.php?action=read&controller=festival&festival_id=$festival_id\"> <span class=\"title\">$festival_name</span></a>
 		<div class=\"secondary-content\">
 			<a title=\"en savoir plus\" href=\"index.php?action=read&controller=festival&festival_id=$festival_id\" class=\"btn\"><i class=\"material-icons\">more</i></a>
 			<a title=\"voir le formulaire\" href=\"index.php?action=create&festival_id=$festival_id\" class=\"btn\"><i class=\"material-icons\">assignment</i></a>
 		</div>
 	</li>";
-				$i++;
-			}
-			echo '<h4>Liste des festivals</h4>';
-			$i = 1;
-			foreach ($tab_f as $f) {
-				$festival_name = htmlspecialchars($f->getFestivalName());
-				$festival_id = $f->getFestivalId();
-				if (!ModelUtilisateur::estResponsable($user_id, $festival_id)) {
-					echo "<li class=\"collection-item\">
+						$i++;
+					}
+					echo '<h4>Liste des festivals</h4>';
+					$i = 1;
+					foreach ($tab_f as $f) {
+						$festival_name = htmlspecialchars($f->getFestivalName());
+						$festival_id = $f->getFestivalId();
+						if (!ModelUtilisateur::estResponsable($user_id, $festival_id)) {
+							echo "<li class=\"collection-item\">
             <a href=\"index.php?action=readForUser&controller=festival&festival_id=$festival_id\"> <span class=\"title\">$festival_name</span></a>
             <div class=\"secondary-content\">
                 <a title=\"en savoir plus\" href=\"index.php?action=readForUser&controller=festival&festival_id=$festival_id\" class=\"btn\"><i class=\"material-icons\">more</i></a>
                 <a title=\"voir le formulaire\" href=\"index.php?action=create&festival_id=$festival_id\" class=\"btn\"><i class=\"material-icons\">assignment</i></a>
             </div>";
+						}
+
+						echo "</li>";
+						$i++;
+					}
 				}
 
-				echo "</li>";
-				$i++;
-			}
-		}
-
-		?>
-</ul>
+				?>
+		</ul>
+	</div>
+</div>
