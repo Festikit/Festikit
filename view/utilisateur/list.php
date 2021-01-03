1d54a75d74e5928c5d505a7bbca7aa0d360bc60d
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-01-03 21:26:21 +0100

    Mise à jour design : liste utilisateur

--- view/utilisateur/list.php
+++ view/utilisateur/list.php
@@ -1,19 +1,22 @@
-<ul class="collection">
-    <li class="collection-header">
-        <h4>Liste de tous les utilisateurs</h4>
-    </li>
-    <?php
-    // Liste tous les utilisateurs de la base de donnée: bénévoles (postulé accepté: oui et non) et les responsables
-    $i = 1;
-    foreach ($tab_u as $u) {
-        $user_id = rawurlencode($u->getId());
-        $user_firstname = htmlspecialchars($u->getFirstname());
-        $user_lastname = htmlspecialchars($u->getLastname());
-        $user_picture = $u->getPicture();
-        echo 
-        '<li class="collection-item avatar">
+<div class="row">
+
+    <div class="card col l8 offset-l2 col m10 offset-m1 col s12">
+        <ul class="collection">
+            <li class="collection-header">
+                <h4>Liste de tous les utilisateurs</h4>
+            </li>
+            <?php
+            // Liste tous les utilisateurs de la base de donnée: bénévoles (postulé accepté: oui et non) et les responsables
+            $i = 1;
+            foreach ($tab_u as $u) {
+                $user_id = rawurlencode($u->getId());
+                $user_firstname = htmlspecialchars($u->getFirstname());
+                $user_lastname = htmlspecialchars($u->getLastname());
+                $user_picture = $u->getPicture();
+                echo
+                    '<li class="collection-item avatar">
             <img class="circle" alt="profil" src="data:image/jpg;base64,' . base64_encode($user_picture) . '" onerror="this.onerror=null; this.src=\'data:image/png;base64,' . base64_encode($user_picture) . '\'" width="70px"/>';
-            echo "
+                echo "
             <a href=\"index.php?action=read&user_id=$user_id\"> <span class=\"title\">$user_firstname</span><p> $user_lastname</p></a>
             <div class=\"secondary-content\">
                 <a title=\"en savoir plus\" href=\"index.php?action=read&user_id=$user_id\" class=\"btn\"><i class=\"material-icons\">more</i></a>
@@ -32,7 +35,9 @@
             </div>
         </li>
 ";
-        $i++;
-    }
-    ?>
-</ul>
+                $i++;
+            }
+            ?>
+        </ul>
+    </div>
+</div>
