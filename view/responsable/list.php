e8ebffdfb1e060d83a5bbf6b16547869ad4ee974
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2021-01-10 02:17:13 +0100

    Revert "Debug responsable"
    This reverts commit 68456390e5646477a25553864245e3d29206655c.

--- view/responsable/list.php
+++ view/responsable/list.php
@@ -1,10 +1,22 @@
 
+<<<<<<< HEAD
 <?php
 	// Liste tous les responsables
 	
 	$i = 1;
         foreach ($tab_nomResponsable as $r) {
                 
+=======
+    <div class="card col l8 offset-l2 col m10 offset-m1 col s12">
+        <ul class="collection">
+            <li class="collection-header">
+                <h4>Liste de tous les responsables</h4>
+            </li>
+            <?php
+            // Liste tous les responsables
+            $i = 1;
+            foreach ($tab_nomResponsable as $r) {
+>>>>>>> parent of 6845639... Debug responsable
                 $user_id = rawurlencode($r->getResponsableId());
 		$user_firstname = htmlspecialchars($r->getUserFirstname());
                 $user_lastname = htmlspecialchars($r->getUserLastname());
